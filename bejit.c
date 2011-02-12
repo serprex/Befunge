966145e6a5e59caca346697f69614f49cc1e1fd6
Demur Rumed <junkmute@hotmail.com>
2011-02-12 22:30:14 +0000

    Simpler flow and data offsets

--- bejit.c
+++ bejit.c
@@ -22,17 +22,11 @@ int opc(int i){
 	static const uint8_t loc[]={16,28,31,17,14,23,36,36,36,12,10,21,11,20,13,0,1,2,3,4,5,6,7,8,9,18,36,34,36,32,27,30,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,36,19,36,33,29,15,36,36,36,36,36,36,24,36,36,36,36,36,36,36,36,25,36,36,36,36,36,35,36,36,36,36,36,26,36,22};
 	return i<33||i>126?36:loc[i-33];
 }
-uint16_t comp(int i){
-	int op=opc(ps[i>>2]);
-	uint32_t cl=0;
-	for(;;){
-		if(pg[i]!=(uint16_t)-1){
-			rl+=3;
-			r[rl-3]=28;
-			return*(uint16_t*)(r+rl-2)=pg[i];
-		}
+int comp(int i){
+	while(pg[i=mv(i)]==(uint16_t)-1){
 		pg[i]=rl;
 		pro[i>>4]|=1<<(i>>1&6);
+		int op=opc(ps[i>>2]);
 		switch(op){
 		default:__builtin_unreachable();
 		case(0 ... 9)
@@ -64,7 +58,8 @@ uint16_t comp(int i){
 			case(22)*++sp=getchar();
 			case(23)scanf("%d",++sp);
 			case(24)
-				if(sp>st){sp--;*sp=*sp<80&&*sp>=0&&sp[1]<25&&sp[1]>=0?ps[*sp*32|sp[1]]:0;}else if(sp==st)*sp=*sp<25&&*sp>=0?ps[*sp]:0;else*++sp=ps[0];
+				if(sp>st){sp--;*sp=*sp<80&&*sp>=0&&sp[1]<25&&sp[1]>=0?ps[*sp*32|sp[1]]:0;}
+				else if(sp==st)*sp=*sp<25&&*sp>=0?ps[*sp]:0;else*++sp=ps[0];
 			}
 		case(25){
 			rl+=3;
@@ -122,7 +117,7 @@ uint16_t comp(int i){
 			r[rl-7]=26;
 			int j=sp>=st&&*sp--;
 			i=i&~3|(op==26?3-j*2:j*2);
-			*(uint16_t*)(r+rl-2)=mv(i^2);
+			*(uint16_t*)(r+rl-2)=i^2;
 			*(uint16_t*)(r+rl-6+2*j)=rl;
 			*(uint16_t*)(r+rl-6+2*!j)=-1;
 		case(30)exit(0);
@@ -132,8 +127,10 @@ uint16_t comp(int i){
 			i=i&~3|op&3;
 		case(36);
 		}
-		op=opc(ps[(i=mv(i))>>2]);
 	}
+	rl+=3;
+	r[rl-3]=28;
+	return*(uint16_t*)(r+rl-2)=pg[i];
 }
 int main(int argc,char**argv){
 	ran=fopen("/dev/urandom","r");
@@ -159,36 +156,36 @@ int main(int argc,char**argv){
 	RunProg:fclose(prog);
 	memset(pro,0,640);
 	memset(pg,-1,20480);
-	uint8_t*op=r+comp(0);
+	uint8_t*op=r+comp(10112);
 	for(;;){
-		switch(*op){
+		switch(*op++){
 		default:__builtin_unreachable();
 		case(0)
-			*++sp=*(int32_t*)(op+1);
+			*++sp=*(int32_t*)op;
 			op+=4;
 		case(1)
-			*++sp=ps[*(uint16_t*)(op+1)];
+			*++sp=ps[*(uint16_t*)op];
 			op+=2;
 		case(2)
 			op+=2;
 			goto from2;
 		case(4)
-			if(sp>=st){*sp=*sp>*(int32_t*)(op+1);}else{st[0]=sp==st&&0>*(int32_t*)(op+1);sp=st;}
+			if(sp>=st)*sp=*sp>*(int32_t*)op;else{st[0]=sp==st&&0>*(int32_t*)op;sp=st;}
 			op+=4;
 		case(5)
-			if(sp>=st){if(*(int32_t*)(op+1))*sp%=*(int32_t*)(op+1);}else*(sp=st)=0;
+			if(sp>=st){if(*(int32_t*)op)*sp%=*(int32_t*)op;}else*(sp=st)=0;
 			op+=4;
 		case(6)
-			if(sp>=st){if(*(int32_t*)(op+1))*sp/=*(int32_t*)(op+1);}else*(sp=st)=0;
+			if(sp>=st){if(*(int32_t*)op)*sp/=*(int32_t*)op;}else*(sp=st)=0;
 			op+=4;
 		case(7)
-			if(sp>=st){*sp*=*(int32_t*)(op+1);}else*(sp=st)=0;
+			if(sp>=st)*sp*=*(int32_t*)op;else*(sp=st)=0;
 			op+=4;
 		case(8)
-			if(sp>=st){*sp-=*(int32_t*)(op+1);}else*(sp=st)=-*(int32_t*)(op+1);
+			if(sp>=st)*sp-=*(int32_t*)op;else*(sp=st)=-*(int32_t*)op;
 			op+=4;
 		case(9)
-			if(sp>=st){*sp+=*(int32_t*)(op+1);}else*(sp=st)=*(int32_t*)(op+1);
+			if(sp>=st)*sp+=*(int32_t*)op;else*(sp=st)=*(int32_t*)op;
 			op+=4;
 		case(10)if(sp>st){sp--;*sp+=sp[1];}else if(sp<st)*++sp=0;
 		case(11)if(sp>st){sp--;*sp-=sp[1];}else if(sp==st)*sp*=-1;else*++sp=0;
@@ -236,40 +233,34 @@ int main(int argc,char**argv){
 				ps[x]=0;
 			}
 			if(0){from2:
-				x=*(uint16_t*)(op-1);
+				x=*(uint16_t*)op;
 				y=ps[x];
 				ps[x]=sp>=st?*sp--:0;
 			}
-			if(y!=ps[x]&&(pro[x>>2]&3<<(x&3)*2)&&((pro[x>>2]&2<<(x&3)*2)||opc(y)!=opc(ps[x]))){
+			if(y!=ps[x]&&pro[x>>2]&3<<(x&3)*2&&(pro[x>>2]&2<<(x&3)*2||opc(y)!=opc(ps[x]))){
 				memset(pro,0,640);
 				memset(pg,-1,20480);
 				rl=0;
-				op=r+comp(mv(*(uint16_t*)(op+1)));
-				continue;
+				op=r+comp(*(uint16_t*)op);
 			}else op+=2;
 		}
 		case(26){
 			int j=sp>=st&&*sp--;
-			uint16_t*i=(uint16_t*)(op+1+j*2);
+			uint16_t*i=(uint16_t*)(op+j*2);
 			if(*i==(uint16_t)-1){
 				*i=rl;
-				op=r+comp(*(uint16_t*)(op+5));
+				op=r+comp(*(uint16_t*)(op+4));
 			}else op=r+*i;
-			continue;
 		}
 		case(27){
 			int j=getc(ran)&3;
-			uint16_t*i=(uint16_t*)(op+1+j*2);
+			uint16_t*i=(uint16_t*)(op+j*2);
 			if(*i==(uint16_t)-1){
 				*i=rl;
-				op=r+comp(mv(*(uint16_t*)(op+9)|j));
+				op=r+comp(*(uint16_t*)(op+8)|j);
 			}else op=r+*i;
-			continue;
 		}
-		case(28)
-			op=r+*(uint16_t*)(op+1);
-			continue;
+		case(28)op=r+*(uint16_t*)op;
 		}
-		op++;
 	}
 }
