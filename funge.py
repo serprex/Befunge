ec7898fb135a1cf570f3241a8dd26e0c26c1ac17
Demur Rumed <gunkmute@gmail.com>
2015-09-29 00:12:54 +0000

    Remove pre0 corpse, start stub for post compile optimize pass, fix p to maintain stack

--- funge.py
+++ funge.py
@@ -18,10 +18,11 @@ def getch():
 	return getch
 getch = getch()
 
-def main(pstring, pop=0):
-	from opcode import opmap,HAVE_ARGUMENT
+def main(pstring, argv=()):
+	from opcode import opmap,HAVE_ARGUMENT,hasjabs
 	from types import CodeType,FunctionType
 	from random import randint
+	hasjabs = frozenset(hasjabs)
 	ps = [0]*2560
 	if debug:print(pstring)
 	pstring = pstring.split("\n")
@@ -38,16 +39,15 @@ def main(pstring, pop=0):
 		pg=[None]*10240
 	initstate()
 	def rmem(x,y):return ps[x<<5|y] if 0<=x<=80 and 0<=y<=25 else 0
-	def wmem(x,y,v):
-		nonlocal r
-		if 0<=x<=80 and 0<=25<=y:
+	def wmem(x,y,v,i):
+		if 0<=x<=80 and 0<=y<=25:
 			y|=x<<5
 			ps[y]=v
 			if getpro(y):
 				initstate()
-				compile(i)
+				compile(i,None)
 				return True
-		return False
+			return False
 	def rng():return randint(0,3)
 	def getop(i):return b'\x10\x1d\x1f\x11\x0e\x17$$$\x0c\n\x15\x0b\x14\r\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\x12$"$ \x1a\x1e$$$$$$$$$$$$$$$$$$$$$$$$$$$\x13$!\x1c\x0f$$$$$$\x18$$$$$$$$\x19$$$$$#$$$$$\x1b$\x16'[i-33] if 33<=i<=126 else 36
 	def emit(op,arg=None):
@@ -84,6 +84,7 @@ def main(pstring, pop=0):
 		r.append(100)
 		emitidx(consts, c)
 	putch = stdout.write
+	putint = lambda x:print(+x,end=' ')
 	def storefast(x):
 		r.append(125)
 		emitarg(x)
@@ -191,8 +192,7 @@ def main(pstring, pop=0):
 		swap()
 	def op20(op,i):
 		spguard(1,-1)
-		emit("UNARY_POSITIVE")
-		loadconst(print)
+		loadconst(putint)
 		swap()
 		popcall(1)
 	def op21(op,i):
@@ -224,10 +224,27 @@ def main(pstring, pop=0):
 		loadconst(wmem)
 		rot3()
 		loadfast(1)
-		call(3)
-		emit("POP_JUMP_IF_FALSE", len(r)+4)
-		emit("RETURN_VALUE")
+		loadconst(i)
+		call(4)
+		j=len(r)
+		emit("POP_JUMP_IF_FALSE", 0)
+		emit("BUILD_LIST",0)
+		loadfast(0)
+		dup()
+		j2=len(r)
+		emit("POP_JUMP_IF_FALSE",0)
+		rot3()
+		swap()
+		emit("BUILD_TUPLE",1)
+		emit("INPLACE_ADD")
+		swap()
+		loadconst(1)
+		emit("BINARY_SUBTRACT")
+		emit("JUMP_ABSOLUTE",j2-1)
 		pop()
+		emit("RETURN_VALUE")
+		i=len(r)
+		patch((j,j2),(i,i-2))
 	def op26(op,i):
 		loadconst(rng)
 		call(0)
@@ -294,18 +311,39 @@ def main(pstring, pop=0):
 				if ni == -1:return ret
 				else:i=ni
 			i=mv(i)
+	def filterempty(r):
+		i=0
+		while i<len(r):
+			op = r[i]
+			n = i+(1 if op<HAVE_ARGUMENT else 3)
+			while i<n:
+				if op:yield r[i]
+				i+=1
+		return r
+	def optimize(r):
+		if "-o" in argv:return r
+		from collections import defaultdict
+		jtbl=defaultdict(list)
+		i=0
+		while i<len(r):
+			op = r[i]
+			if op in hasjabs:
+				jtbl[op].append(i)
+			i+=1 if op<HAVE_ARGUMENT else 3
+		return filterempty(r)
 	compile(10112,None)
 	def prog():
 		do=True
-		while do:
-			f=FunctionType(CodeType(0,0,2,65536,0,bytes(r),tuple(consts),(),("s","t"),"","",0,b""),{})
-			if debug>1:
+		while do != 0:
+			if do is not True:r[:0]=do[::-1]
+			f=FunctionType(CodeType(0,0,2,65536,0,bytes(optimize(r)),tuple(consts),(),("s","t"),"","",0,b""),{})
+			if debug>1 or "dis" in argv:
 				from dis import dis
 				dis(f)
 			do=f()
+			if debug:print(do)
 	return prog
 if __name__ == "__main__":
 	from sys import argv
-	pre0 = -int(argv[2]) if len(argv) > 2 and argv[2].isdigit() else 0
 	debug = argv.count("d")
-	main(open(argv[1]).read(),pre0)()
+	main(open(argv[1]).read(),argv)()
