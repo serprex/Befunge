d9380e4479e0d9f54a99586d1251d99a950d19bc
Demur Rumed <gunkmute@gmail.com>
2018-01-27 23:14:52 +0000

    quire.js changed

--- js/quire.js
+++ js/quire.js
@@ -1,11 +1,4 @@
-"use strict"
-var require=(function(o){var M={},a=/[^/]+\/\.\.\//,r=function(x,p){
-x=(p||"")+x.replace(/^\.\//,"")
-while(x.match(a))x=x.replace(a,"")
-return(M[x]||o)(x)}
-r.f=function(p,f){var m={},j=p.lastIndexOf("/"),s=~j?p.slice(0,j+1):""
-M[p]=function(){return m.exports||(f(m,m.exports={},function(x){return r(x,s)}),m.exports)}}
-return r})(typeof require!="undefined"&&require);require.f('js/funge',function(module,exports,require){"use strict";
+require.f('js/funge',function(module,exports,require){});"use strict";
 function varint (v, value) {
 	while (true) {
 		let b = value & 127;
@@ -1089,4 +1082,11 @@ exports.runSource = function(board, imp, interp){
 	}
 	bfRun(imp, 10112, 0, interp);
 }
-});
+"use strict"
+var require=(function(o){var M={},a=/[^/]+\/\.\.\//,r=function(x,p){
+x=(p||"")+x.replace(/^\.\//,"")
+while(x.match(a))x=x.replace(a,"")
+return(M[x]||o)(x)}
+r.f=function(p,f){var m={},j=p.lastIndexOf("/"),s=~j?p.slice(0,j+1):""
+M[p]=function(){return m.exports||(f(m,m.exports={},function(x){return r(x,s)}),m.exports)}}
+return r})(typeof require!="undefined"&&require);
